dc4e0c59c33750be85b207c6370e4da13293a19b
eitetsu-yo-BM <e-yo@bluememe.jp>
2024-08-15 19:17:38 +0900

    RealtyOffterType Obj changed and calculate commission

--- UsrRealty/Schemas/UsrRealty_FormPage/UsrRealty_FormPage.js
+++ UsrRealty/Schemas/UsrRealty_FormPage/UsrRealty_FormPage.js
@@ -82,7 +82,7 @@ define("UsrRealty_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHE
 					"visible": true,
 					"icon": "process-button-icon",
 					"clicked": {
-						"request": "usr.PushButtonRequest" //crt.SaveRecordRequest
+						"request": "usr.PushButtonRequest"
 					},
 					"clickMode": "default"
 				},
@@ -147,6 +147,25 @@ define("UsrRealty_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHE
 				"parentName": "SideAreaProfileContainer",
 				"propertyName": "items",
 				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "Commission",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrCommissionUSD_fgw6f08",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrCommissionUSD_fgw6f08"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 3
 			},
 			{
 				"operation": "insert",
@@ -305,6 +324,28 @@ define("UsrRealty_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHE
 				"parentName": "GeneralInfoTabContainer",
 				"propertyName": "items",
 				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "CommissionPercent",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PDS_UsrOfferTypeUsrCommissionPercent",
+					"control": "$PDS_UsrOfferTypeUsrCommissionPercent",
+					"readonly": true,
+					"placeholder": "",
+					"labelPosition": "auto",
+					"tooltip": ""
+				},
+				"parentName": "GeneralInfoTabContainer",
+				"propertyName": "items",
+				"index": 5
 			}
 		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
 		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
@@ -353,6 +394,16 @@ define("UsrRealty_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHE
 						"modelConfig": {
 							"path": "PDS.UsrNumber"
 						}
+					},
+					"PDS_UsrOfferTypeUsrCommissionPercent": {
+						"modelConfig": {
+							"path": "PDS.UsrOfferTypeUsrCommissionPercent"
+						}
+					},
+					"PDS_UsrCommissionUSD_fgw6f08": {
+						"modelConfig": {
+							"path": "PDS.UsrCommissionUSD"
+						}
 					}
 				}
 			},
@@ -385,7 +436,13 @@ define("UsrRealty_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHE
 					"PDS": {
 						"type": "crt.EntityDataSource",
 						"config": {
-							"entitySchemaName": "UsrRealty"
+							"entitySchemaName": "UsrRealty",
+							"attributes": {
+								"UsrOfferTypeUsrCommissionPercent": {
+									"path": "UsrOfferType.UsrCommissionPercent",
+									"type": "ForwardReference"
+								}
+							}
 						},
 						"scope": "page"
 					}
@@ -394,21 +451,32 @@ define("UsrRealty_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHE
 		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
 		handlers: /**SCHEMA_HANDLERS*/[
          {
-            request: "usr.PushButtonRequest",				
-            /* Implementation of the custom query handler. */				
-            handler: async (request, next) => {					
-              console.log("Button works...");					
-              
-              Terrasoft.showInformation("My button was pressed.");					
-              var price = await request.$context.PDS_UsrPriceUSD_vfy36iu;					
-              console.log("Price = " + price);					
-              
-              request.$context.PDS_UsrArea_z824zj0 = price * 0.5;					
-              
-              /* Call the next handler if it exists and return its result. */					
-              return next?.handle(request);				
-            }			
+            request: "usr.PushButtonRequest",	
+            /* Implementation of the custom query handler. */				
+            handler: async (request, next) => {					
+              console.log("Button works...");					             
+              Terrasoft.showInformation("My button was pressed.");					
+              var price = await request.$context.PDS_UsrPriceUSD_vfy36iu;					
+              console.log("Price = " + price);					              
+              request.$context.PDS_UsrArea_z824zj0 = price * 0.5;					            
+              /* Call the next handler if it exists and return its result. */					
+              return next?.handle(request);	
+            }
           },
+          {				
+           request: "crt.HandleViewModelAttributeChangeRequest",					
+           handler: async (request, next) => {	
+             /* if price changed or percent changed */
+             if (request.attributeName === 'PDS_UsrPriceUSD_vfy36iu' ||  request.attributeName === 'PDS_UsrOfferTypeUsrCommissionPercent' ) {	       
+               var price = await request.$context.PDS_UsrPriceUSD_vfy36iu;						
+               var percent = await request.$context.PDS_UsrOfferTypeUsrCommissionPercent;						
+               var commission = price * percent / 100;						
+               request.$context.PDS_UsrCommissionUSD_fgw6f08 = commission;					
+             }					
+             /* Call the next handler if it exists and return its result. */					
+             return next?.handle(request);				
+           }			
+         }
         ]/**SCHEMA_HANDLERS*/,
 		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
 		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
